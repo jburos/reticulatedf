52e4650f55ade6c67fbd95a364e2ac300a5dcd82
Jacqueline Buros <jacki@hammerlab.org>
2017-05-03 21:35:12 +0000

    remove system-specific path

--- tests/testthat/test_py_to_r_df.R
+++ tests/testthat/test_py_to_r_df.R
@@ -2,7 +2,6 @@
 library(reticulate)
 library(testthat)
 library(dplyr)
-reticulate::use_condaenv('root', conda = '/home/analysis/miniconda3/bin/conda', required = T) # local testing
 
 skip_if_no_numpy <- function() {
   have_numpy <- reticulate::py_module_available("numpy")
